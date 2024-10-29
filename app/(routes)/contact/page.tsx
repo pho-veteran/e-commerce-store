3a24472114094d652d3d1de103d86a2797417175
pho-veteran <128946325+pho-veteran@users.noreply.github.com>
2024-10-29 21:16:23 +0700

    Dev Stage | Search

---
+++ app/(routes)/contact/page.tsx
@@ -0,0 +1,15 @@
+import Container from "@/components/ui/container";
+
+const ContactPage = () => {
+    return (
+        <div>
+            <Container>
+                <div
+                    className="px-4 sm:px-6 lg:px-8"
+                >Contact Page</div>
+            </Container>
+        </div>
+    );
+}
+
+export default ContactPage;
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { ShoppingBag } from "lucide-react";
+import { Search, ShoppingBag } from "lucide-react";
 import { useEffect, useState } from "react";
 
 import useCart from "@/hooks/use-cart";
 import Button from "@/components/ui/Button";
 import { useRouter } from "next/navigation";
 import CartHover from "./cart-hover";
+import IconButton from "./ui/icon-button";
 
 const NavbarActions = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -21,6 +22,10 @@ const NavbarActions = () => {
 
     return (
         <div className="ml-auto flex items-center gap-x-4">
+            <IconButton 
+                icon={<Search size={20} />}
+                className="border-none shadow-none"
+            />
             <CartHover>
                 <Button className="flex items-center rounded-full bg-black px-4 py-2" onClick={() => {
                     router.push("/cart");
