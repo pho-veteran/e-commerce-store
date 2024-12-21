e4839c83bb8c686ebd5b020f18294d833d5d8e1f
pho-veteran <128946325+pho-veteran@users.noreply.github.com>
2024-12-21 22:59:18 +0700

    Deployment | ESLint Error Fixes

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,7 +2,11 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  className?: string;
+};
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
@@ -20,3 +24,4 @@ const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLI
 Input.displayName = "Input"
 
 export { Input }
+export type { InputProps }
